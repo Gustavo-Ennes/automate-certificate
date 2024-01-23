9163318ac5d01f0e6f5ba81f5fc79513675c3825
Gustavo-Ennes <gustavo.ennes@3778.care>
2024-01-23 17:10:26 -0300

    first commit

---
+++ src/app.py
@@ -0,0 +1,29 @@
+
+import openpyxl
+from PIL import Image, ImageDraw
+
+from draw import draw_certificate
+
+WORKBOOK_PATH = './table/certificate.xlsx'
+SHEET_NAME = 'students'
+IMAGE_PATH = "./source_image/certificate_background.jpg"
+
+workbook = openpyxl.load_workbook(WORKBOOK_PATH)
+data = workbook[SHEET_NAME]
+
+def parse_data(student):
+    return {
+      'name': student[0].value,
+      'cpf': student[1].value,
+      'course_name': student[2].value,
+      'course_date': student[3].value
+    }
+
+for index, student in enumerate(data.iter_rows(min_row=2)):
+  parsed_data = parse_data(student)  
+  image = Image.open(IMAGE_PATH)  
+  drawer = ImageDraw.Draw(image)
+  
+  draw_certificate(drawer, parsed_data, image)  
+  
+  image.save(f'./certificates/{parsed_data["name"]}.png', 'PNG')
