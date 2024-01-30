0599fca6d9e3e5c5e454bacebb792d2b97c4cfda
Gustavo-Ennes <gustavo.ennes@3778.care>
2024-01-30 07:18:18 -0300

    Pep8 adjustments

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+"""Module to automate certificate creation based on a spreadsheet"""
 
 import openpyxl
 from PIL import Image, ImageDraw
@@ -11,19 +12,22 @@ IMAGE_PATH = "./source_image/certificate_background.jpg"
 workbook = openpyxl.load_workbook(WORKBOOK_PATH)
 data = workbook[SHEET_NAME]
 
-def parse_data(student):
+
+def parse_data(certificate_data):
+    """Method to parse data to certificate"""
     return {
-      'name': student[0].value,
-      'cpf': student[1].value,
-      'course_name': student[2].value,
-      'course_date': student[3].value
+        'name': certificate_data[0].value,
+        'cpf': certificate_data[1].value,
+        'course_name': certificate_data[2].value,
+        'course_date': certificate_data[3].value
     }
 
+
 for index, student in enumerate(data.iter_rows(min_row=2)):
-  parsed_data = parse_data(student)  
-  image = Image.open(IMAGE_PATH)  
-  drawer = ImageDraw.Draw(image)
-  
-  draw_certificate(drawer, parsed_data, image)  
-  
-  image.save(f'./certificates/{parsed_data["name"]}.png', 'PNG')
+    parsed_data = parse_data(student)
+    image = Image.open(IMAGE_PATH)
+    drawer = ImageDraw.Draw(image)
+
+    draw_certificate(drawer, parsed_data, image)
+
+    image.save(f'./certificates/{parsed_data["name"]}.png', 'PNG')
--- src/draw.py
+++ src/draw.py
@@ -1,3 +1,5 @@
+"""Module to provide function to draw text in certificate"""
+
 from io import BytesIO
 from PIL import ImageFont, Image
 
@@ -6,7 +8,7 @@ PAGE_X_SIZE = 730
 NAME_FONT_SIZE = 55
 TITLE_FONT_SIZE = 65
 NORMAL_FONT_SIZE = 18
-BOLD_FONT_PATH ="./fonts/Weather-Sunday.otf"
+BOLD_FONT_PATH = "./fonts/Weather-Sunday.otf"
 REGULAR_FONT_PATH = "./fonts/DejaVuSans.ttf"
 TITLE_FONT_PATH = "./fonts/vogue.ttf"
 COLOR = 'black'
@@ -18,128 +20,152 @@ title_font_file = open(TITLE_FONT_PATH, "rb")
 bold_bytes_font = BytesIO(bold_font_file.read())
 reg_bytes_font = BytesIO(reg_font_file.read())
 title_bytes_font = BytesIO(title_font_file.read())
-name_font = ImageFont.truetype(bold_bytes_font,NAME_FONT_SIZE)
+name_font = ImageFont.truetype(bold_bytes_font, NAME_FONT_SIZE)
 title_font = ImageFont.truetype(title_bytes_font, TITLE_FONT_SIZE)
 normal_font = ImageFont.truetype(reg_bytes_font, NORMAL_FONT_SIZE)
 
-    
-def getCenterPointX(text, font, drawer):
-  _, _, w, _ = drawer.textbbox((0, 0), text, font)
-  return (PAGE_X_SIZE - w)/2
+
+def get_center_point_x(text, font, drawer):
+    """get the horizontal center of the screen"""
+    _, _, w, _ = drawer.textbbox((0, 0), text, font)
+    return (PAGE_X_SIZE - w)/2
 
 
-def draw_name(drawer, name):  
-  centralized_coordinates = (getCenterPointX(name, name_font, drawer), 130)
-  drawer.text(centralized_coordinates, name, fill=COLOR, font=name_font)
-  
-def draw_cpf(drawer, cpf):  
-  text = f'CPF: {cpf}'
-  centralized_coordinates = (getCenterPointX(str(text), normal_font, drawer), 200)
-  drawer.text(centralized_coordinates, str(text), fill=COLOR, font=normal_font)
-  
+def draw_name(drawer, name):
+    """draw certificate name"""
+    centralized_coordinates = (
+        get_center_point_x(name, name_font, drawer), 130)
+    drawer.text(centralized_coordinates, name, fill=COLOR, font=name_font)
+
+
+def draw_cpf(drawer, cpf):
+    """draw certificate document"""
+    text = f'CPF: {cpf}'
+    centralized_coordinates = (get_center_point_x(
+        str(text), normal_font, drawer), 200)
+    drawer.text(centralized_coordinates, str(
+        text), fill=COLOR, font=normal_font)
+
+
 def draw_curse_date(drawer, course_date):
-  drawer.text((320, 470), course_date.strftime("%d/%m/%Y"), fill=COLOR, font=normal_font)
-  
+    """draw certificate date"""
+    drawer.text((320, 470), course_date.strftime(
+        "%d/%m/%Y"), fill=COLOR, font=normal_font)
+
+
 def draw_title(drawer):
-  certificate_title = "C E R T I F I C A D O"
-  center_x = getCenterPointX(certificate_title, title_font, drawer)
-  drawer.text(
-    (center_x, 45), 
-    certificate_title, 
-    fill=COLOR, 
-    font=title_font
-  )
+    """draw certificate title"""
+    certificate_title = "C E R T I F I C A D O"
+    center_x = get_center_point_x(certificate_title, title_font, drawer)
+    drawer.text(
+        (center_x, 45),
+        certificate_title,
+        fill=COLOR,
+        font=title_font
+    )
+
 
 def draw_first_line(drawer):
-  text = "Este certificado atesta que"
-  center_x = getCenterPointX(text, normal_font, drawer)
-  drawer.text(
-    (center_x, 120), 
-    text, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  
+    """draw certificate first line"""
+    text = "Este certificado atesta que"
+    center_x = get_center_point_x(text, normal_font, drawer)
+    drawer.text(
+        (center_x, 120),
+        text,
+        fill=COLOR,
+        font=normal_font
+    )
+
+
 def draw_text_block(drawer, parsed_data):
-  phrase1 = f'concluiu com êxito o curso de {parsed_data["course_name"]} oferecido '
-  phrase2 = 'pela instituição Kratos Cursos SA, com início na data de  '
-  phrase3 = f'{parsed_data["course_date"].strftime("%d/%m/%Y")}. Durante sua participação no curso, demonstrou'
-  phrase4 = 'um notável comprometimento e dedicação,destacando-se pelo'
-  phrase5 = 'seu empenho notável em todas as atividades propostas.'
-  getCenter = lambda text: getCenterPointX(text, normal_font, drawer)  
-  drawer.text(
-    (getCenter(phrase1), 240), 
-    phrase1, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  drawer.text(
-    (getCenter(phrase2), 260), 
-    phrase2, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  drawer.text(
-    (getCenter(phrase3), 280), 
-    phrase3, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  drawer.text(
-    (getCenter(phrase4), 300), 
-    phrase4 , 
-    fill=COLOR, 
-    font=normal_font
-  )
-  drawer.text(
-    (getCenter(phrase5), 320), 
-    phrase5, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  
-def draw_signatures(drawer):  
-  line = "________________________"
-  left_text = "Diretor"
-  right_text = 'Coordenador'
-  getCenterLeft = lambda text: getCenterPointX(text, normal_font, drawer) / 2  
-  getCenterRight = lambda text: getCenterPointX(text, normal_font, drawer) * 1.5 
-  
-  drawer.text(
-    (getCenterLeft(line), 380), 
-    line, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  drawer.text(
-    (getCenterLeft(left_text) + 30, 400), 
-    left_text, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  drawer.text(
-    (getCenterRight(line), 380), 
-    line, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  drawer.text(
-    (getCenterRight(right_text) - 25, 400), 
-    right_text, 
-    fill=COLOR, 
-    font=normal_font
-  )
-  
-def draw_seal(image):  
-  seal_raw = Image.open(SEAL_PATH)
-  seal = seal_raw.resize((130, 100))
-  image.paste(seal, (600,400), seal)
-  
+    """draw certificate text block"""
+    phrase1 = f'concluiu com êxito o curso de {parsed_data["course_name"]} oferecido '
+    phrase2 = 'pela instituição Kratos Cursos SA, com início na data de  '
+    phrase3 = f'{parsed_data["course_date"].strftime("%d/%m/%Y")}. Durante sua participação no curso, demonstrou'
+    phrase4 = 'um notável comprometimento e dedicação,destacando-se pelo'
+    phrase5 = 'seu empenho notável em todas as atividades propostas.'
+    get_center = lambda text:get_center_point_x(text, normal_font, drawer)
+
+    drawer.text(
+        (get_center(phrase1), 240),
+        phrase1,
+        fill=COLOR,
+        font=normal_font
+    )
+    drawer.text(
+        (get_center(phrase2), 260),
+        phrase2,
+        fill=COLOR,
+        font=normal_font
+    )
+    drawer.text(
+        (get_center(phrase3), 280),
+        phrase3,
+        fill=COLOR,
+        font=normal_font
+    )
+    drawer.text(
+        (get_center(phrase4), 300),
+        phrase4,
+        fill=COLOR,
+        font=normal_font
+    )
+    drawer.text(
+        (get_center(phrase5), 320),
+        phrase5,
+        fill=COLOR,
+        font=normal_font
+    )
+
+
+def draw_signatures(drawer):
+    """draw certificate signature spaces"""
+    line = "________________________"
+    left_text = "Diretor"
+    right_text = 'Coordenador'
+
+    get_center_left = lambda text: get_center_point_x(text, normal_font, drawer) / 2
+    get_center_right = lambda text: get_center_point_x(text, normal_font, drawer) * 1.5
+
+    drawer.text(
+        (get_center_left(line), 380),
+        line,
+        fill=COLOR,
+        font=normal_font
+    )
+    drawer.text(
+        (get_center_left(left_text) + 30, 400),
+        left_text,
+        fill=COLOR,
+        font=normal_font
+    )
+    drawer.text(
+        (get_center_right(line), 380),
+        line,
+        fill=COLOR,
+        font=normal_font
+    )
+    drawer.text(
+        (get_center_right(right_text) - 25, 400),
+        right_text,
+        fill=COLOR,
+        font=normal_font
+    )
+
+
+def draw_seal(image):
+    """draw certificate seal"""
+    seal_raw = Image.open(SEAL_PATH)
+    seal = seal_raw.resize((130, 100))
+    image.paste(seal, (600, 400), seal)
+
+
 def draw_certificate(drawer, data, image):
-  draw_seal(image)
-  draw_title(drawer)
-  draw_first_line(drawer)
-  draw_name(drawer, data['name'])
-  draw_cpf(drawer, data['cpf'])
-  draw_text_block(drawer, data)
-  draw_signatures(drawer)
+    """draw certificate routine"""
+    draw_seal(image)
+    draw_title(drawer)
+    draw_first_line(drawer)
+    draw_name(drawer, data['name'])
+    draw_cpf(drawer, data['cpf'])
+    draw_text_block(drawer, data)
+    draw_signatures(drawer)
